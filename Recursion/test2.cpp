3855309db18a9e5402ae8be5ad956f2c66cef3d6
Sarvjot <sarvjotmalhotra@gmail.com>
2025-02-18 17:29:41 +0530

    new

---
+++ Recursion/test2.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int cnt =0;
+void print(int i, int n){
+   
+    if(i>n) return;
+    // to print couting
+    // cout<<i<<endl;
+    // to print anything
+    cout<<" Sarvjot Singh "<<endl;
+    print(i+1, n);
+}
+int main(){
+    int n;
+    cout<<"Enter N ";
+    cin>>n;
+
+    print(1, n);
+}
